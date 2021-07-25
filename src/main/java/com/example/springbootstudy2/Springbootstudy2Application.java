84ec617fb708a44b8984fc867df8f8d2531c6a5b
tjwjdgks <tjwjdgks43@gmail.com>
2021-07-25 17:14:16 +0900

    first commit

---
+++ src/main/java/com/example/springbootstudy2/Springbootstudy2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootstudy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootstudy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootstudy2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootstudy2/Springbootstudy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootstudy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootstudy2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
