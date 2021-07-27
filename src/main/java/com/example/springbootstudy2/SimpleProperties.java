d697ba32504d4904e8a956f90bff24f7a74d4e1b
tjwjdgks <tjwjdgks43@gmail.com>
2021-07-27 15:39:44 +0900

    spring 외부 설정

---
+++ src/main/java/com/example/springbootstudy2/SimpleProperties.java
@@ -0,0 +1,61 @@
+package com.example.springbootstudy2;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.convert.DurationUnit;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+// 프로퍼티 bean 으로 등록
+// getter setter 필요하다 자동 바인딩
+// meta 정보 생성 플러그인 도움을 받는다 autoconfigure-processor
+// type converstion 된다
+// 시간 정보도 가능
+@Component
+@ConfigurationProperties("simple")
+@Validated
+public class SimpleProperties {
+    @NotEmpty
+    private int age;
+    private String name;
+    private String full;
+
+    @DurationUnit(ChronoUnit.SECONDS)
+    private Duration sessiontime = Duration.ofSeconds(30);
+
+    public Duration getSessiontime() {
+        return sessiontime;
+    }
+
+    public void setSessiontime(Duration sessiontime) {
+        this.sessiontime = sessiontime;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFull() {
+        return full;
+    }
+
+    public void setFull(String full) {
+        this.full = full;
+    }
+}
--- src/main/java/com/example/springbootstudy2/SimpleRunner.java
+++ src/main/java/com/example/springbootstudy2/SimpleRunner.java
@@ -1,16 +1,32 @@
 package com.example.springbootstudy2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleRunner implements ApplicationRunner {
+    // 외부 라이브러리에서도 properties 가져올 수 있음 but 이미 bean으로 바인딩 되므로 사실상 유의미 x
+    @ConfigurationProperties("server")
+    @Bean
+    public ServerProperties serverProperties(){
+        return new ServerProperties();
+    }
+    @Autowired
+    SimpleProperties properties;
     @Value("${seo.name}")
     private String name;
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("simpleRunner " + name);
+        System.out.println(properties.getName());
+        System.out.println(properties.getAge());
+        System.out.println(properties.getSessiontime());
+
     }
 }
